4478bd09c3ab3ebb14061c8d4fe66104f7dadcf4
maade93791 <70593890+maade69@users.noreply.github.com>
2026-05-04 02:04:20 +0300

    HideCarrierInfo: remove dead code

--- core/java/android/ext/carrierinfo/HideCarrierInfo.java
+++ core/java/android/ext/carrierinfo/HideCarrierInfo.java
@@ -1,20 +1,15 @@
 package android.ext.carrierinfo;
 
-import android.app.ActivityManager;
 import android.app.AppGlobals;
 import android.app.Application;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.GosPackageState;
 import android.ext.settings.app.AswHideCarrierInfo;
-import android.os.RemoteException;
 import android.os.UserHandle;
-import android.util.Log;
 
 /** @hide */
 public class HideCarrierInfo {
-    private static final String TAG = HideCarrierInfo.class.getSimpleName();
-
     /** @hide */
     public static final int FLAG_HIDE_CARRIER_INFO = 1;
 
@@ -46,40 +41,6 @@ public class HideCarrierInfo {
     /** @hide */
     public static boolean isEnabled() {
         return (flags & FLAG_HIDE_CARRIER_INFO) != 0;
-    }
-
-    /** @hide */
-    public static boolean shouldFilter(String key) {
-        if (!isEnabled() || key == null) {
-            return false;
-        }
-        var filter = key.startsWith("gsm.sim.") || key.startsWith("gsm.operator.");
-        if (filter) {
-            reportAccess(key);
-        }
-        return filter;
-    }
-
-    /** @hide */
-    public static boolean hideFromSelf(String apiName) {
-        if (!isEnabled()) {
-            return false;
-        }
-        reportAccess(apiName);
-        return true;
-    }
-
-    private static void reportAccess(String apiName) {
-        Application app = AppGlobals.getInitialApplication();
-        if (app == null) {
-            return;
-        }
-        try {
-            ActivityManager.getService()
-                    .showCarrierInfoAccessNotification(app.getPackageName(), apiName);
-        } catch (RemoteException e) {
-            Log.d(TAG, "", e);
-        }
     }
 
     private HideCarrierInfo() {}
